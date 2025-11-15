b6836e46cf1d7555a3a245b10418f06ed6261bd8
kathiravan292 <kkathiravan785@gmail.com>
2025-11-15 18:18:24 +0530

    url

--- src/util/config.js
+++ src/util/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL="https://blogserver-ruo1.onrender.com/api/v1"
+export const BASE_URL=" https://blogserver-ruo1.onrender.com/api/v1"
 
 export const TOKEN = localStorage.getItem("token") 
 export const ROLE = localStorage.getItem("role")
