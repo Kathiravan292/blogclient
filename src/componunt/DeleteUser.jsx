bd5c76ca6068b68546b942e952ad5e25e5493d77
kathiravan C <124439980+Kathiravan292@users.noreply.github.com>
2025-12-08 12:45:40 +0530

    Update DeleteUser.jsx

--- src/componunt/DeleteUser.jsx
+++ src/componunt/DeleteUser.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import  { useEffect, useState } from 'react'
 import { data, useNavigate, useParams } from 'react-router-dom'
-import { BASE_URL, TOKEN } from '../util/config'
+import { BASE_URL, TOKEN } from '../util/config' 
 
 
 const DeleteUser = () => {
